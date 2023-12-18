478dfd6c9debc1f292ed5acb0e4f497f8c19ab38
exkazuu-wb <154104105+exkazuu-wb@users.noreply.github.com>
2023-12-18 02:30:30 +0000

    fix: update table

--- Hello.py
+++ Hello.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+"Hello World!"
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'first column': [1, 2, 3, 4, 5],
+  'second column': [10, 20, 30, 40, 50]
 })
 
 df
